c39a4e402a52baf27863d413d44612c1d59e693b
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2022-04-14 11:50:48 +0200

    created portfolio page

---
+++ script.js
@@ -0,0 +1,107 @@
+
+  //Navbar appear on scroll
+    window.addEventListener('scroll', function () {
+        let header = document.querySelector('header');
+
+        header.classList.toggle('scrolling-active', window.scrollY > 0);
+    })
+
+    //Text fade in on scroll
+    const fadeIn = document.querySelectorAll('.fade-in');
+    const appearOptions = {
+        threshold: 1
+    };
+
+    const appearOnScroll = new IntersectionObserver
+    (function(
+        entries, 
+        appearOnScroll
+        ) {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    return;
+                } else {
+                    entry.target.classList.add('is-visible');
+                }
+            })
+        }, 
+    appearOptions);
+
+    fadeIn.forEach(fadeIn => {
+        appearOnScroll.observe(fadeIn);
+    })
+
+    //Slide in on scroll
+
+    const txtAppearOptions = {
+        threshold: 1
+    };
+
+    const slideLeft = document.querySelectorAll('.slide-left');
+
+    const slideLeftOnScroll = new IntersectionObserver
+    (function(
+        entries,
+        slideLeftOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('slideLeft');
+            }
+        })
+    },
+        txtAppearOptions);
+    
+        slideLeft.forEach(slideLeft => {
+        slideLeftOnScroll.observe(slideLeft);
+    })
+
+    const slideRight = document.querySelectorAll('.slide-right');
+
+    const slideRightOnScroll = new IntersectionObserver
+    (function(
+        entries,
+        slideRightOnScroll
+    ) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('slideRight');
+            }
+        })
+    },
+        txtAppearOptions);
+    
+        slideRight.forEach(slideRight => {
+        slideRightOnScroll.observe(slideRight);
+    })
+
+
+
+    //Img fade in on scroll
+    const imgFadeIn = document.querySelectorAll('.img-animated');
+    const imgAppearOptions = {
+        threshold: 1
+    };
+
+    const imgAppearOnScroll = new IntersectionObserver
+    (function(
+        entries, 
+        appearOnScroll
+        ) {
+            entries.forEach(entry => {
+                if (!entry.isIntersecting) {
+                    return;
+                } else {
+                    entry.target.classList.add('is-visible');
+                }
+            })
+        }, 
+    imgAppearOptions);
+
+    imgFadeIn.forEach(fadeIn => {
+        imgAppearOnScroll.observe(fadeIn);
+    })
