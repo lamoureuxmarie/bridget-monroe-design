8f84551911e18ef790e9f608f39e784722586c5b
Marie Lamoureux <69575238+lamoureuxmarie@users.noreply.github.com>
2022-04-19 17:42:45 +0200

    created "expand more content" function

--- script.js
+++ script.js
@@ -79,4 +79,21 @@
         slideRightOnScroll.observe(slideRight);
     })
 
+    //Show More buttons
+
+    const expandsMore = document.querySelectorAll('[expand-more]')
+
+    function expand() {
+        const showContent = document.getElementById(this.dataset.target)
+        if (showContent.classList.contains('expand-active')) {
+            this.innerHTML=this.dataset.showtext
+        } else {
+            this.innerHTML=this.dataset.hidetext
+        }
+        showContent.classList.toggle('expand-active')
+        }
+
+    expandsMore.forEach(expandMore => {
+        expandMore.addEventListener('click', expand)
+    })
 
